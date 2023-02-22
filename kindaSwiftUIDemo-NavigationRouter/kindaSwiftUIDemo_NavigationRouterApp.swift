bf82225e4d321158e962164dfc292d9947eb52cd
rebeloper <therebeloper@gmail.com>
2023-02-22 18:20:57 +0200

    Initial Commit

---
+++ kindaSwiftUIDemo-NavigationRouter/kindaSwiftUIDemo_NavigationRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  kindaSwiftUIDemo_NavigationRouterApp.swift
+//  kindaSwiftUIDemo-NavigationRouter
+//
+//  Created by Alex Nagy on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct kindaSwiftUIDemo_NavigationRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
